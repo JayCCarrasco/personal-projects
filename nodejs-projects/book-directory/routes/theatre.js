9e7eb44b7ca3680a7a6f531302dd881b37d28e84
jayccarrasco <JayCCarrasco@users.noreply.github.com>
2023-06-20 06:23:04 +0200

    adding theatre books routes. methods.http modified

--- nodejs-projects/book-directory/routes/theatre.js
+++ nodejs-projects/book-directory/routes/theatre.js
@@ -9,4 +9,42 @@ theatreRouter.get(('/'), (req, res)=>{
     res.send(theatre);
 })
 
+theatreRouter.get(('/:author'), (req, res) => {
+    let author = req.params.author;
+    let theatreFilter = theatre.filter(book => book.author === author);
+    if (theatreFilter.length === 0){
+        return res.status(404).send(`There are no authors with name: ${author}`);
+    }
+    res.send(JSON.stringify(theatreFilter));
+})
+
+theatreRouter.post(('/'), (req, res) => {
+    let bookPost = req.body;
+    theatre.push(bookPost);
+    res.send(JSON.stringify(theatre));
+})
+
+//put method
+theatreRouter.put(('/'), (req, res) => {
+    let bookChange = req.body;
+    let indexChange = theatre.findIndex(book => book.id === bookChange.id)
+    if (indexChange === -1){
+        return res.status(404).send(`The id ${bookChange.id} does not exist`);
+    }
+    theatre.splice(indexChange, 1, bookChange);
+    res.send(JSON.stringify(theatre));
+})
+
+theatreRouter.delete(('/:id'), (req, res) => {
+    let bookDeleteID = req.params.id;
+    let index = theatre.findIndex(book => book.id == bookDeleteID);
+    console.log(index)
+    if (index === -1){
+        return res.status(404).send(`No ID: ${bookDeleteID}`);
+    }
+    theatre.splice(index,1);
+    res.send(JSON.stringify(theatre));
+
+})
+
 module.exports = theatreRouter;
